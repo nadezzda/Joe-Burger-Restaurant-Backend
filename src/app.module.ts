e13a286e0c9b102d47083b815a22ed2b6aca03b1
Nadiia <malicenkonadic@gmail.com>
2023-10-26 20:57:24 +0300

    Create review model

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UserModule } from './user/user.module';
 import { MenuItemModule } from './menu-item/menu-item.module';
 import { CategoryModule } from './category/category.module';
 import { OrderModule } from './order/order.module';
+import { ReviewModule } from './review/review.module';
 
 @Module({
-  imports: [UserModule, MenuItemModule, CategoryModule, OrderModule],
+  imports: [UserModule, MenuItemModule, CategoryModule, OrderModule, ReviewModule],
   controllers: [AppController],
   providers: [AppService],
 })
