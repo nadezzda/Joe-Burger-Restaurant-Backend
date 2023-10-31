71614186ccdff8a35ef258d7620850702e62437e
Nadiia <malicenkonadic@gmail.com>
2023-10-31 15:23:53 +0200

    Create siqnup logic

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,19 @@ import { MenuItemModule } from './menu-item/menu-item.module';
 import { CategoryModule } from './category/category.module';
 import { OrderModule } from './order/order.module';
 import { ReviewModule } from './review/review.module';
+import { AuthModule } from './auth/auth.module';
+import { PrismaService } from './prisma.service';
 
 @Module({
-  imports: [UserModule, MenuItemModule, CategoryModule, OrderModule, ReviewModule],
+  imports: [
+    UserModule,
+    MenuItemModule,
+    CategoryModule,
+    OrderModule,
+    ReviewModule,
+    AuthModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
